9c8fb60b2ebbd45d24dcc9e0b7423bfc237c6320
Jack Quinoa <jquinoa208@gmail.com>
2020-06-10 00:20:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student < User 
-
+  
+  
+  def initialize(first, last)
+    @first_name = first 
+    @last_name = last 
+    @@knowledge = []
+  end
+  
+  def learn(piece_of_knowledge) 
+    @@knowledge << piece_of_knowledge
+  end
+  
+  def knowledge
+    @@knowledge
+  end
+  
 end
